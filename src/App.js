8970f74869c2535fd29e6b5c5400f79cc95be168
Nosov-Pavel <nopaev@gmail.com>
2020-10-22 13:24:07 -0400

    create

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
 import React, {useState} from "react";
 import './App.css';
-import CounterList from "./CounterList";
-import Controller from "./Controller";
-import F1 from "./F1"
+
 
 function App() {
-    const initCounters = [0, 0, 0];
-    const [counters, setCounters] = useState([0, 0, 0]);
+    const [list, setList] = useState([
+        {value: 0, id: 1},
+        {value: 1, id: 2},
+        {value: 2, id: 3},
+    ])
 
-    const addCounter = () => {
-        const newCounters = [...counters];
-        newCounters.push(0);
-
-        setCounters(newCounters);
+    const addButton = () => {
+        const newList = [...list, 1];
+        setList(newList);
     }
 
-    const counterReset = () => {
-        setCounters(initCounters);
-    }
-    const counterChange = (index, val) => {
-        const newCounters = counters.map( (el, i) => {
-            if (index === i) return el + val
 
-            return  el
-        })
-        setCounters(newCounters);
-    }
-
-    const deleteCounter = (index) => {
-        const newCounter = counters.filter((el,i) => i !== index)
-        setCounters(newCounter);
-    }
 
     return (
         <div>
-            <Controller addCounter={addCounter()} counterReset={{counterReset}}/>
-            <CounterList deleteCounter={deleteCounter} counterChange={counterChange} list={counters}/>
-
-
+            <button onClick={addButton}>create</button>
+            <hr/>
+            {list.map(el =>
+            <div key={el.id}>
+                <button>-</button>
+                {el.value}
+                <button>+</button>
+            </div>)}
         </div>
     );
 }
--- src/CounterList.js
+++ src/CounterList.js
@@ -5,15 +5,7 @@ function CounterList(props){
 
     return(
         <div>
-            {props.list.map((el, i) => <div key={Math.random()}>
-                <button onClick={() => props.counterChange(i,-1)}>-1</button>
-                <button onClick={() => props.counterChange(i,-2)}>-2</button>
-                <button onClick={() => props.counterChange(i,-3)}>-3</button>
-                {el}
-                <button onClick={() => props.counterChange(i,1)}>+1</button>
-                <button onClick={() => props.counterChange(i,2)}>+2</button>
-                <button onClick={() => props.counterChange(i,3)}>+3</button>
-                <button onClick={() => props.deleteCounter(i)}>delete</button>
+            {props.list.map((el) => <div
             </div>)}
 
 
