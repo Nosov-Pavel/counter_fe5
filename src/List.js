eca9239830ad21798b05496be8dc733446b942f3
Nosov-Pavel <nopaev@gmail.com>
2020-12-04 12:05:09 -0500

    List

---
+++ src/List.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+
+function List() {
+    const [noute, setNoute] = useState()
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+export default List;
