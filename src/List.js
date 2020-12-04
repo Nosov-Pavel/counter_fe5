f2a36bc9f00fa413b9765a1152df19d9080139c9
Nosov-Pavel <nopaev@gmail.com>
2020-12-04 12:26:09 -0500

    setNN

--- src/List.js
+++ src/List.js
@@ -3,12 +3,15 @@ import React, {useState} from "react";
 
 function List() {
     const [noute, setNoute] = useState('Apple')
+    const setNewNoute = () => {
+        setNoute('newNoute')
+    }
 
     return (
         <div>
             {noute}
             <input type="text"/>
-            <button></button>
+            <button onClick={setNewNoute}>set</button>
 
         </div>
     );
