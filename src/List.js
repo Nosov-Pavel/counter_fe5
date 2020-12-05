49597f9ed1e77830943d164fa713ba00806b23d9
Nosov-Pavel <nopaev@gmail.com>
2020-12-05 16:44:56 -0500

    onChange

--- src/List.js
+++ src/List.js
@@ -4,16 +4,21 @@ import React, {useState} from "react";
 function List() {
     const [noute, setNoute] = useState('Apple')
     const setNewNoute = () => {
+
         setNoute('newNoute')
+    }
+    const onInputChange = () => {
+        console.log('Hello')
     }
 
     return (
         <div>
             {noute}
-            <input type="text"/>
+            <input type="text" onChange={onInputChange}/>
             <button onClick={setNewNoute}>set</button>
 
         </div>
     );
 }
+
 export default List;
